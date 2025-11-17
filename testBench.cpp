819ca9d34261c7a2bcdf0af423543de96c14a81c
Li He <He.Li@harman.com>
2025-11-17 14:53:19 +0800

    add testbench

---
+++ testBench.cpp
@@ -0,0 +1,3 @@
+1 MdnsDiscoveredDevice( const MdnsDiscoveredDevice & )            = delete;
+2 MSG_OBJECT_ACTIVATED *pMsg = new ( pData ) MSG_OBJECT_ACTIVATED( length, id, static_cast< HM_UINT32 >( data.size() ) );
+3 MSG_OBJECT_LIST *pMsg = new ( pData ) MSG_OBJECT_LIST;
